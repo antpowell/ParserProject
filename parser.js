d000fe21d3bfd137aa223cddb3525d93eacde00a
Powell, Anthony <powell.anthony.e@gmail.com>
2017-12-12 20:24:02 -0600

    init commit

---
+++ parser.js
@@ -0,0 +1,46 @@
+/* Grammer:
+  <OPERATION> -> <ANDCHECK><OR>
+  <OR>        -> v <ANDCHECK><OR> | E
+  <ANDCHECK>  -> <DIGITCHECK><AND>
+  <AND>       -> ^ <DIGITCHECK><AND> | E
+  <DIGITCHECK>-> NOT<DIGIT> | <DIGIT>
+  <DIGIT>     -> (<OPERATION>) | 1 | 0 
+  */
+
+  /* This program will check if a given language is accepted by the grammer rules above.
+    This program should take a string as input and return a boolean as output as well as display the steps
+    taken to check if the language is accepted. 
+  */
+(function(){
+  function reader(){
+
+  }
+
+  function printer(){
+      
+  }
+
+  function operation(){
+
+  }
+
+  function or(){
+
+  }
+
+  function andCheck(){
+
+  }
+
+  function and(){
+
+  }
+
+  function digitCheck(){
+
+  }
+
+  function digit(){
+
+  }
+})()
